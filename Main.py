f1d6fe21d4715ed4b9e21feb5047495ef52af266
maldo31 <74554376+maldo31@users.noreply.github.com>
2021-05-14 22:25:08 +0200

    added sort dict, and sum

--- Main.py
+++ Main.py
@@ -11,9 +11,23 @@ def count_occurance(text):
             dict[char]=1
     return dict
 
+def sortDict(to_sort):
+    sorted_dict = {}
+    sorted_keys = sorted(to_sort, key=to_sort.get,reverse=True)  # [1, 3, 2]
+    for w in sorted_keys:
+        sorted_dict[w]=to_sort[w]
+    return sorted_dict
+
+
+
+
 file = readFromFile("kod.txt")
 dictionary=count_occurance(file)
+sorted=sortDict(dictionary)
+counted=sum(sorted.values())
 print(file)
 print("\n")
 print(dictionary)
+print(sorted)
+print(counted)
 
